651a90b8f07eca536beffc5d6499cf07f4098add
VidwaDeSeram <vidwadeseram2002@gmail.com>
2023-05-06 14:31:55 +0530

    all done

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ResponsiveAppBar from "./Components/appBar";
+
+const App = () => {
+  return (
+    <div>
+      <ResponsiveAppBar />
+      {/* Other components, if any */}
+    </div>
+  );
+};
+
+export default App;
